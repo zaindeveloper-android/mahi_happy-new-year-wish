6689aeb7a79ee071455d099c3426e8c4dbe52888
zaindeveloper-android <abdulla786972@gmail.com>
2026-01-01 21:03:29 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function nextPage(page) {
+    window.location.href = page;
+}
+
+function notForgiven() {
+    alert(
+        "Mahi 🥺❤️\n\n" +
+        "please roya na karo...\n\n" +
+        "jab tum roti ho na,\n" +
+        "mujhe bohot zyada dukh hota hai 💔\n" +
+        "main khud ko bilkul theek feel nahi karta...\n\n" +
+        "jo bhi baat ho,\n" +
+        "jo bhi tension ho,\n" +
+        "mujhse share kar liya karo 🤍\n\n" +
+        "please ab maan jao...\n" +
+        "tumhari ek smile mere liye sab kuch hai ❤️"
+    );
+}
+
+function thankYou() {
+    alert(
+        "💖 Thank you Mahi…\n\n" +
+        "you are so much special for me ❤️\n" +
+        "Happy New Year 2026 🎊"
+    );
+}
